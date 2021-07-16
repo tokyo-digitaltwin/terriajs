230ca4b8d12b4bdac6396545f684eb14ed75c736
Yusuke Kiuchi <4221178-yusuke.kiuchi@users.noreply.gitlab.com>
2021-07-16 23:27:08 +0900

    Don't show "Show Raw Data" button

--- lib/ReactViews/FeatureInfo/FeatureInfoSection.jsx
+++ lib/ReactViews/FeatureInfo/FeatureInfoSection.jsx
@@ -281,7 +281,11 @@ export const FeatureInfoSection = observer(
           </If>
           <If condition={this.props.isOpen}>
             <section className={Styles.content}>
-              <If condition={!this.props.printView && this.hasTemplate()}>
+              <If
+                condition={
+                  !this.props.printView && this.hasTemplate() && false // Disable show raw data
+                }
+              >
                 <button
                   type="button"
                   className={Styles.rawDataButton}
