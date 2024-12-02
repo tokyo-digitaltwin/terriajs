0fd3858297cc53ece903877cf80d3c18cfdaea88
monomoti <takashige.nakagawa@gmail.com>
2024-12-02 10:51:20 +0900

    11/29リリースの更新内容 - 3Dモードでの標高COG不具合の修正 - Geojsonで白丸がジオイド付近に表示される不具合の修正 - KMLの属性情報表示不具合の修正

--- lib/Models/Catalog/CatalogItems/CogCatalogItem.ts
+++ lib/Models/Catalog/CatalogItems/CogCatalogItem.ts
@@ -1,189 +1,244 @@
-import { computed } from "mobx";
-import isDefined from "../../../Core/isDefined";
+import i18next from "i18next";
+import {
+  computed,
+  makeObservable,
+  observable,
+  onBecomeObserved,
+  onBecomeUnobserved,
+  runInAction
+} from "mobx";
+import {
+  GeographicTilingScheme,
+  WebMercatorTilingScheme
+} from "terriajs-cesium";
+import CesiumMath from "terriajs-cesium/Source/Core/Math";
+import type TIFFImageryProvider from "terriajs-tiff-imagery-provider";
 import CatalogMemberMixin from "../../../ModelMixins/CatalogMemberMixin";
 import MappableMixin, { MapItem } from "../../../ModelMixins/MappableMixin";
 import CogCatalogItemTraits from "../../../Traits/TraitsClasses/CogCatalogItemTraits";
+import { RectangleTraits } from "../../../Traits/TraitsClasses/MappableTraits";
 import CreateModel from "../../Definition/CreateModel";
+import LoadableStratum from "../../Definition/LoadableStratum";
+import { BaseModel } from "../../Definition/Model";
+import StratumFromTraits from "../../Definition/StratumFromTraits";
+import StratumOrder from "../../Definition/StratumOrder";
+import Terria from "../../Terria";
 import proxyCatalogItemUrl from "../proxyCatalogItemUrl";
-import TIFFImageryProvider, {
-  TIFFImageryProviderOptionsWithUrl
-} from "../../../ThirdParty/tiff-imagery-provider";
-import Credit from "terriajs-cesium/Source/Core/Credit";
-import Proj4Definitions from "../../../Map/Vector/Proj4Definitions";
-import Reproject from "../../../Map/Vector/Reproject";
-import { ImageryProvider } from "terriajs-cesium";
-import { LatLngBounds } from "leaflet";
-const proj4 = require("proj4").default;
-const parseGeoRaster = require("georaster");
-import { GeoRaster } from "georaster-layer-for-leaflet";
-import { IPromiseBasedObservable, fromPromise } from "mobx-utils";
-import GeorasterTerriaLayer from "../../../Map/Leaflet/GeorasterTerriaLayer";
-import { mapElevationToRgbaSmoothed } from "../../../Core/colourMappings";
 
+/**
+ * Loadable stratum for overriding CogCatalogItem traits
+ */
+class CogLoadableStratum extends LoadableStratum(CogCatalogItemTraits) {
+  static stratumName = "cog-loadable-stratum";
+
+  constructor(readonly model: CogCatalogItem) {
+    super();
+    makeObservable(this);
+  }
+
+  duplicateLoadableStratum(model: BaseModel): this {
+    return new CogLoadableStratum(model as CogCatalogItem) as this;
+  }
+
+  @computed
+  get shortReport(): string | undefined {
+    // We will not show models.commonModelErrors.3dTypeIn2dMode even in 2D mode.
+    return this.model.terria.currentViewer.type === "Leaflet" && false
+      ? // Warn for 2D mode
+        i18next.t("models.commonModelErrors.3dTypeIn2dMode", this)
+      : this.model._imageryProvider?.tilingScheme &&
+        // Show warning for experimental reprojection freature if not using EPSG 3857 or 4326
+        isCustomTilingScheme(this.model._imageryProvider?.tilingScheme)
+      ? i18next.t("models.cogCatalogItem.experimentalReprojectionWarning", this)
+      : undefined;
+  }
+
+  @computed
+  get rectangle(): StratumFromTraits<RectangleTraits> | undefined {
+    const rectangle = this.model._imageryProvider?.rectangle;
+    if (!rectangle) {
+      return;
+    }
+
+    const { west, south, east, north } = rectangle;
+    return {
+      west: CesiumMath.toDegrees(west),
+      south: CesiumMath.toDegrees(south),
+      east: CesiumMath.toDegrees(east),
+      north: CesiumMath.toDegrees(north)
+    };
+  }
+}
+
+StratumOrder.addLoadStratum(CogLoadableStratum.stratumName);
+
+/**
+ * Creates a Cloud Optimised Geotiff catalog item.
+ *
+ * Currently it can render EPSG 4326/3857 COG files. There is experimental
+ * support for other projections, however it is less performant and could have
+ * unknown issues.
+ */
 export default class CogCatalogItem extends MappableMixin(
   CatalogMemberMixin(CreateModel(CogCatalogItemTraits))
 ) {
   static readonly type = "cog";
 
+  /**
+   * Private imageryProvider instance. This is set once forceLoadMapItems is
+   * called.
+   */
+  @observable
+  _imageryProvider: TIFFImageryProvider | undefined;
+
+  /**
+   * The reprojector function to use for reprojecting non native projections
+   *
+   * Exposed here as instance variable for stubbing in specs.
+   */
+  reprojector = reprojector;
+
   get type() {
     return CogCatalogItem.type;
   }
 
-  protected async forceLoadMapItems(): Promise<void> {
-    // return this.forceLoadMetadata();
-    await this.imageryProvider?.readyPromise;
+  constructor(
+    id: string | undefined,
+    terria: Terria,
+    sourceReference?: BaseModel | undefined
+  ) {
+    super(id, terria, sourceReference);
+    makeObservable(this);
+    this.strata.set(
+      CogLoadableStratum.stratumName,
+      new CogLoadableStratum(this)
+    );
 
-    return Promise.resolve();
+    // Destroy the imageryProvider when `mapItems` is no longer consumed. This
+    // is so that the webworkers and other resources created by the
+    // imageryProvider can be freed. Ideally, there would be a more explicit
+    // `destroy()` method in Terria life-cycle so that we don't have to rely on
+    // mapItems becoming observed or unobserved.
+    onBecomeUnobserved(this, "mapItems", () => {
+      if (this._imageryProvider) {
+        this._imageryProvider.destroy();
+        this._imageryProvider = undefined;
+      }
+    });
+
+    // Re-create the imageryProvider if `mapItems` is consumed again after we
+    // destroyed it
+    onBecomeObserved(this, "mapItems", () => {
+      if (!this._imageryProvider && !this.isLoadingMapItems) {
+        this.loadMapItems(true);
+      }
+    });
+  }
+
+  protected async forceLoadMapItems(): Promise<void> {
+    if (!this.url) {
+      return;
+    }
+    const url = proxyCatalogItemUrl(this, this.url);
+    const imageryProvider = await this.createImageryProvider(url);
+    runInAction(() => {
+      this._imageryProvider = imageryProvider;
+    });
   }
 
   @computed get mapItems(): MapItem[] {
-    const imageryProvider = this.imageryProvider;
-
-    if (!isDefined(imageryProvider) || this.isLoadingMapItems) {
+    const imageryProvider = this._imageryProvider;
+    if (!imageryProvider) {
       return [];
     }
+
     return [
       {
         show: this.show,
         alpha: this.opacity,
+        // The 'requestImage' method in Cesium's ImageryProvider has a return type that is stricter than necessary.
+        // In our custom ImageryProvider, we return ImageData, which is also a valid return type.
+        // However, since the current Cesium type definitions do not reflect this flexibility, we use a TypeScript ignore comment ('@ts-ignore')
+        // to suppress the type checking error. This is a temporary solution until the type definitions in Cesium are updated to accommodate ImageData.
+        // @ts-expect-error - The return type of 'requestImage' method in our custom ImageryProvider can be ImageData, which is not currently allowed in Cesium's type definitions, but is fine.
         imageryProvider,
-        clippingRectangle: imageryProvider.rectangle,
-        // Define our method for generating a leaflet layer in a different way, here
-        overrideCreateLeafletLayer: this.createGeoRasterLayer
+        clippingRectangle: this.cesiumRectangle
       }
     ];
   }
 
-  @computed get georasterLayer(): IPromiseBasedObservable<
-    GeorasterTerriaLayer | undefined
-  > {
-    return fromPromise(
-      // parseGeoRaster will request for external .ovr file, most likely will receive a 404 error. This is not a problem.
-      isDefined(this.imageryProvider) &&
-        parseGeoRaster(this.url)
-          .then((georaster: GeoRaster) => {
-            return new GeorasterTerriaLayer(
-              this.terria.leaflet,
-              {
-                georaster: georaster,
-                opacity: 1,
-                // Example pixel reclassification function:
-                // pixelValuesToColorFn: (values) => {
-                //   return mapElevationToRgbaSmoothed(values, 0);
-                // },
-                resolution: 256
-                // debugLevel: 0
-              },
-              this.imageryProvider
-            );
-          })
-          .catch((error: Error) => {
-            this.terria.raiseErrorToUser(error);
-          })
-    );
-  }
-
-  createGeoRasterLayer = (
-    ip: ImageryProvider,
-    clippingRectangle: LatLngBounds | undefined
-  ): GeorasterTerriaLayer | undefined => {
-    return this.url && this.georasterLayer.state === "fulfilled"
-      ? this.georasterLayer.value
-      : undefined;
-  };
-
   /**
-   * Handle all different possible projections of COGs
-   * @param code Should be a number representing an EPSG code
-   * @returns a Promise that resolves to a proj reprojection function
+   * Create TIFFImageryProvider for the given url.
    */
-  // TODO: This needs to return an object with a project and an unproject function
-  projFunc = (code: number) => {
-    const sourceEpsgCode = `EPSG:${code}`;
-    // Add the projection to our proj4 defs if we dont already have it:
-    const project = Reproject.checkProjection(
-      this.terria.configParameters.proj4ServiceBaseUrl,
-      sourceEpsgCode
-    )
-      .then(() => {
-        const sourceDef =
-          sourceEpsgCode in Proj4Definitions
-            ? new proj4.Proj(Proj4Definitions[sourceEpsgCode])
-            : undefined;
+  private async createImageryProvider(
+    url: string
+  ): Promise<TIFFImageryProvider> {
+    // lazy load the imagery provider, only when needed
+    const [{ default: TIFFImageryProvider }, { default: proj4 }] =
+      await Promise.all([
+        import("terriajs-tiff-imagery-provider"),
+        import("proj4-fully-loaded")
+      ]);
 
-        return proj4(sourceDef, "EPSG:4326").forward;
+    return runInAction(() =>
+      TIFFImageryProvider.fromUrl(url, {
+        credit: this.credit,
+        tileSize: this.tileSize,
+        maximumLevel: this.maximumLevel,
+        minimumLevel: this.minimumLevel,
+        enablePickFeatures: this.allowFeaturePicking,
+        hasAlphaChannel: this.hasAlphaChannel,
+        // used for reprojecting from an unknown projection to 4326/3857
+        // note that this is experimental and could be slow as it runs on the main thread
+        projFunc: this.reprojector(proj4),
+        // make sure we omit `undefined` options so as not to override the library defaults
+        renderOptions: omitUndefined({
+          nodata: this.renderOptions.nodata,
+          convertToRGB: this.renderOptions.convertToRGB,
+          resampleMethod: this.renderOptions.resampleMethod
+        })
       })
-      .catch((error: Error) => {
-        this.terria.raiseErrorToUser(error);
-      });
-
-    const unproject = Reproject.checkProjection(
-      this.terria.configParameters.proj4ServiceBaseUrl,
-      sourceEpsgCode
-    )
-      .then(() => {
-        const sourceDef =
-          sourceEpsgCode in Proj4Definitions
-            ? new proj4.Proj(Proj4Definitions[sourceEpsgCode])
-            : undefined;
-
-        return proj4("EPSG:4326", sourceDef).forward;
-      })
-      .catch((error: Error) => {
-        this.terria.raiseErrorToUser(error);
-      });
-
-    return { project, unproject };
-  };
-
-  @computed get imageryProvider() {
-    if (!isDefined(this.url)) {
-      return;
-    }
-
-    // TODO: Where should we declare these?
-    // TODO: Should we make these applicable to both new CogImageryProvider() and new GeorasterLayer()?
-    const cogOptions: TIFFImageryProviderOptionsWithUrl = {
-      url: proxyCatalogItemUrl(this, this.url),
-      projFunc: this.projFunc,
-      renderOptions: {
-        /** nodata value, default read from tiff meta */
-        nodata: 0
-      },
-      enablePickFeatures: this.allowFeaturePicking
-    };
-
-    let cogImageryProvider: CogImageryProvider = new CogImageryProvider(
-      cogOptions
     );
-
-    return cogImageryProvider;
   }
 }
 
-export class CogImageryProvider extends TIFFImageryProvider {
-  // Set values to please poor cesium types
-  defaultNightAlpha = undefined;
-  defaultDayAlpha = undefined;
-  hasAlphaChannel = true;
-  defaultAlpha = <any>undefined;
-  defaultBrightness = <any>undefined;
-  defaultContrast = <any>undefined;
-  defaultGamma = <any>undefined;
-  defaultHue = <any>undefined;
-  defaultSaturation = <any>undefined;
-  defaultMagnificationFilter = undefined as any;
-  defaultMinificationFilter = undefined as any;
-  proxy = <any>undefined;
-  tileDiscardPolicy = <any>undefined;
+/**
+ * Function returning a custom reprojector
+ */
+function reprojector(proj4: any) {
+  return (code: number) => {
+    if (![4326, 3857, 900913].includes(code)) {
+      try {
+        const prj = proj4("EPSG:4326", `EPSG:${code}`);
+        if (prj)
+          return {
+            project: prj.forward,
+            unproject: prj.inverse
+          };
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  };
+}
 
-  getTileCredits(x: number, y: number, level: number): Credit[] {
-    return [];
-  }
+/**
+ * Returns true if the tilingScheme is custom
+ */
+function isCustomTilingScheme(tilingScheme: Object) {
+  // The upstream library defines a TIFFImageryTillingScheme but it is not
+  // exported so we have to check if it is not one of the standard Cesium
+  // tiling schemes. Also, because TIFFImageryTillingScheme derives from
+  // WebMercatorTilingScheme, we cannot simply do an `instanceof` check, we
+  // compare the exact constructor instead.
+  return (
+    tilingScheme.constructor !== WebMercatorTilingScheme &&
+    tilingScheme.constructor !== GeographicTilingScheme
+  );
+}
 
-  constructor(options: TIFFImageryProviderOptionsWithUrl) {
-    super(options);
-
-    // We can extend the constructor here if needed
-  }
+function omitUndefined(obj: Object) {
+  return Object.fromEntries(
+    Object.entries(obj).filter(([_, value]) => value !== undefined)
+  );
 }
