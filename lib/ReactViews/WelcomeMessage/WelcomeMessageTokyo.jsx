2dc4e97b5e564c8bbbd8d439a20890b861036c8f
monomoti <takashige.nakagawa@gmail.com>
2025-03-14 00:26:33 +0900

    同意画面を初回のみ表示するようにした。

--- lib/ReactViews/WelcomeMessage/WelcomeMessageTokyo.jsx
+++ lib/ReactViews/WelcomeMessage/WelcomeMessageTokyo.jsx
@@ -65,8 +65,8 @@ class WelcomeMessage extends React.Component {
         !viewState.terria.getLocalProperty(LOCAL_PROPERTY_KEY)) ||
       false;
 
-    // this.props.viewState.setShowWelcomeMessage(shouldShow);
-    this.props.viewState.setShowWelcomeMessage(true);
+    this.props.viewState.setShowWelcomeMessage(shouldShow);
+    // this.props.viewState.setShowWelcomeMessage(true);
   }
 
   render() {
@@ -123,7 +123,7 @@ export const WelcomeMessagePure = (props) => {
       return;
     }
     viewState.terria.setLocalProperty("useCookie", accept);
-    handleClose(false);
+    handleClose(true);
   };
 
   // useKeyPress("Escape", () => {
